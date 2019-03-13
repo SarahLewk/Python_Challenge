08b849f44b3b42600b84ed1182cd0f77379f7992
Sarahlewk <sblewkowicz@gmail.com>
2019-03-12 23:37:13 -0700

    add updated file

--- PyBank/Resources/Main.py
+++ PyBank/Resources/Main.py
@@ -3,9 +3,13 @@ import csv
 
 csvpath = os.path.join('..',"Resources","budget_data.csv")
 
+# Should I be using DictReader?
+#with open(csvpath, newline="") as csvfile:
+#csvreader=csv.DictReader(csvfile)
+
 
 with open(csvpath, newline="") as csvfile:
-    csvreader=csv.DictReader(csvfile)
+    csvreader=csv.reader(csvfile)
     
 #My attempts at #1.......
 
@@ -19,41 +23,37 @@ with open(csvpath, newline="") as csvfile:
 #My 2nd attempt:
     Month_list = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     unique_month_counter = []
-    Profit_Losses = ['Profit','Losses']
-    Loss_Count = []
+    #Profit_Losses = ['Profit','Losses']
+    #Loss_Count = []
     
     for i in Month_list:
         if i not in unique_month_counter:
-             unique_month_counter.append(i)
-             unique_month_counter=len(unique_month_counter)
+            unique_month_counter.append(i)
+            unique_month_counter2=len(unique_month_counter)
+            print("Total Months: ",int(unique_month_counter2))         
+        break
+# I feel like I'm so close but when I run this I get: "Total Months: 1" it appears it is only reading one value in the first cell and not reading the whole column.
 
-        print("Total Months: ", int(totalMonths))
-# When I run this I get: "Total Months: 1" it appears it is only reading one value in the first cell and not reading the whole column.
 
-
-#My attempt at #2: ......(Net Total of Profit/Losses)        
+#My attempt at #2: ......(Net Total of Profit/Losses)       
 #got error when trying to use float
-netprofit=0
-
-with open(csvpath, newline="") as csvfile:
-    csvreader=csv.reader(csvfile)
-    
+netprofit=0   
 
     for row in csvreader:
         netprofit += int(row[1])
         print(netprofit)
 
         break
-# (Error):     Traceback (most recent call last):
-#              File "Main.py", line 21, in <module>
-#              netprofit += int(row[1])
-#              ValueError: invalid literal for int() with base 10: 'Profit/Losses'
+# (No sure what error means):  Traceback (most recent call last):
+#                              netprofit += int(row[1])
+#                              ValueError: invalid literal for int() with base 10: 'Profit/Losses'
 
-#My attempt at #3:.......
-#Avg pf change in Profit/Losses-Sum of column B sum()
+#3:.......
+#Use a for loop to calculate the average change looping through each month
 
-#My attempt at #4:.......
+
+#4:.......
 #Greatest increase in profits use max() function
 
-#My attempt at #5:.......
+#5:.......
 #Greatest decrease in profits use min() function
